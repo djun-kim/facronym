94de7493bcfb5cacfeedaecc3592198341a8c998
Djun Kim <info@cielosystems.com>
2013-08-05 23:04:27 -0700

    Cleaned up code; added some documentation; improved some function names.  Enabled toggled display of debugging information.

--- js/facronym.js
+++ js/facronym.js
@@ -1,18 +1,22 @@
 function AcroCntrl($scope) {
-    $scope.dictionary = {
-	entries: [
-	    {acronym: "GCA", 
-	     expansion: "Global Cloud Alliance", 
-	     id: 1, 
-	     editorEnabled: false},
-	    {acronym: "FAQ", 
-	     expansion: "Frequently Asked Questions", 
-	     id: 2, 
-	     editorEnabled: false}
-	],
-    }
 
-  $scope.editorEnabled = false;
+  // The dictionary model. The entries are here only to provide 
+  // an example - actual data is loaded from a data source.
+  $scope.dictionary = {
+    entries: [
+      {acronym: "GCA", 
+       expansion: "Global Cloud Alliance", 
+       id: 1, 
+       editorEnabled: false},
+      {acronym: "FAQ", 
+       expansion: "Frequently Asked Questions", 
+       id: 2, 
+       editorEnabled: false}
+    ],
+  }
+
+  $scope.loadEntries = function() {
+  };
 
   $scope.createEntry = function() {
       var dict = $scope.dictionary;
@@ -22,7 +26,6 @@ function AcroCntrl($scope) {
   };
 
   $scope.deleteEntry = function(entry) {
-    // alert("deleteEntry("+entry+")");
     var dict = $scope.dictionary;
     for (var i = 0, ii = dict.entries.length; i < ii; i++) {
       if (entry === dict.entries[i].id) {
@@ -31,51 +34,54 @@ function AcroCntrl($scope) {
     }
   };
 
-  $scope.editEntry = function(entry) {
-      // alert("editEntry("+entry+")");
-      var dict = $scope.dictionary;
-      $scope.enableEditor(entry);
+  $scope.saveEntry = function(entry) {
+    var entries = $scope.dictionary.entries;
+
+    i = 0;
+    while ( (i < entries.length) && (entries[i].id !== entry)) { i++; };
+    if (entries[i].id === entry) {
+      entries[i].acronym = entries[i].newentry.acronym;
+      entries[i].expansion = entries[i].newentry.expansion;
+    }
+    $scope.disableEditor(entry);
   };
  
+  /**
+   * Enables the editing for the entry given by the parameter "entry".
+   * @param {Integer} entry The id of the entry to able edit on.
+   */
+  $scope.enableEditor = function(entry) {
+    var entries = $scope.dictionary.entries;
 
-    $scope.enableEditor = function(entry) {
-	// alert("enableEditor(" + entry + ")");
-	var entries = $scope.dictionary.entries;
+    i = 0;
+    while ( (i < entries.length) && (entries[i].id !== entry)) { i++; };
+    if (entries[i].id === entry) {
+      entries[i].editorEnabled = true;
+      entries[i].newentry = {};
+      entries[i].newentry.acronym = entries[i].acronym;
+      entries[i].newentry.expansion = entries[i].expansion;
+    }
+  };
 
-	i = 0;
-	while ( (i < entries.length) && (entries[i].id !== entry)) { i++; };
-	if (entries[i].id === entry) {
-	    entries[i].editorEnabled = true;
-	    entries[i].newentry = {};
-	    entries[i].newentry.acronym = entries[i].acronym;
-	    entries[i].newentry.expansion = entries[i].expansion;
-	}
-    };
+  /**
+   * Disables the editing for the entry given by the parameter "entry".
+   * @param {Integer} entry The id of the entry to disable edit on.
+   */
+  $scope.disableEditor = function(entry) {
+    var entries = $scope.dictionary.entries;
 
-    $scope.disableEditor = function(entry) {
-	// alert("enableEditor(" + entry + ")");
-	var entries = $scope.dictionary.entries;
+    i = 0;
+    while ( (i < entries.length) && (entries[i].id !== entry)) { i++; };
+      if (entries[i].id === entry) {
+        entries[i].editorEnabled = false;
+	delete entries[i].newentry;
+      }
+  };
 
-	i = 0;
-	while ( (i < entries.length) && (entries[i].id !== entry)) { i++; };
-	if (entries[i].id === entry) {
-	    entries[i].editorEnabled = false;
-	    delete entries[i].newentry;
-	}
-    };
+  $scope.debugEnabled = false;
+  $scope.toggleDebug = function() {
+    $scope.debugEnabled = !$scope.debugEnabled;
+  }
 
-    $scope.save = function(entry) {
-	var entries = $scope.dictionary.entries;
-
-	i = 0;
-	while ( (i < entries.length) && (entries[i].id !== entry)) { i++; };
-	if (entries[i].id === entry) {
-	    entries[i].acronym = entries[i].newentry.acronym;
-	    entries[i].expansion = entries[i].newentry.expansion;
-	}
-
-
-	$scope.disableEditor(entry);
-    };
 }
 
